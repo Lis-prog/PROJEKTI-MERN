b1ef4641e622abf75780787bd2b075d0805135be
Lis-prog <lispruthi@gmail.com>
2024-02-27 00:00:56 +0100

    Updated the code

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,51 +1,107 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const doctorSchema = new mongoose.Schema({
+//     firstName: {
+//         type: String,
+//         required: [true, 'Emri duhet shenuar']
+//     },
+//     lastName: {
+//         type: String,
+//         required: [true, 'Mbiemri duhet shenuar']
+//     },
+//     phone: {
+//         type: String,
+//         required: [true, 'Telefoni nuk duhet shenuar']
+//     },
+//     email: {
+//         type: String,
+//         required: [true, 'Email duhet shenuar']
+//     },
+//     website: {
+//         type: String,
+//     },
+//     address: {
+//         type: String,
+//         required: [true, 'Address duhet shenuar'],
+//     },
+//     specialization: {
+//         type: String,
+//         required: [true, 'Specalizimi duhet shenuar'],
+//     },
+//     expirience: {
+//         type: String,
+//         required: [true, 'Eksperienca duhet shenuar'],
+//     },
+//     feesPerCunsaltation: {
+//         type: Number,
+//         required: [true, 'Fee duhet shenuar'],
+//     },
+//     status: {
+//     type: String,
+//     default: 'pending',
+//     },
+//     timings: {
+//         type:Object,
+//         required: [true, 'Koha e punes duhet shenuar'],
+//     },
+// },{timestamps:true}
+// )
+
+// const doctorModel = mongoose.model('doctors', doctorSchema)
+// module.exports = doctorModel
+const mongoose = require("mongoose");
 
 const doctorSchema = new mongoose.Schema({
+    userId: {
+    type: String,
+    },
     firstName: {
-        type: String,
-        required: [true, 'Emri duhet shenuar']
+      type: String,
+      required: [true, "first name is required"],
     },
     lastName: {
-        type: String,
-        required: [true, 'Mbiemri duhet shenuar']
+      type: String,
+      required: [true, "last name is required"],
     },
     phone: {
-        type: String,
-        required: [true, 'Telefoni nuk duhet shenuar']
+      type: String,
+      required: [true, "phone no is required"],
     },
     email: {
-        type: String,
-        required: [true, 'Email duhet shenuar']
+      type: String,
+      required: [true, "email is required"],
     },
     website: {
-        type: String,
+      type: String,
     },
     address: {
-        type: String,
-        required: [true, 'Address duhet shenuar']
+      type: String,
+      required: [true, "address is required"],
     },
     specialization: {
-        type: String,
-        required: [true, 'Specalizimi duhet shenuar']
+      type: String,
+      required: [true, "specialization is require"],
     },
-    expirience: {
-        type: String,
-        required: [true, 'Eksperienca duhet shenuar']
+    experience: {
+      type: String,
+      required: [true, "experience is required"],
     },
     feesPerCunsaltation: {
-        type: Number,
-        required: [true, 'Fee duhet shenuar']
+      type: Number,
+      required: [true, "fee is required"],
     },
     status: {
-    type: String,
-    default: 'pending'
+      type: String,
+      default: "pending",
     },
     timings: {
-        type:Object,
-        required: [true, 'Koha e punes duhet shenuar']
+      type: Object,
+      required: [true, "wrok timing is required"],
     },
-},{timestamps:true}
-)
+  },
+  { timestamps: true }
+);
 
-const doctorModel = mongoose.model('doctors', doctorSchema)
-module.exports = doctorModel
+const doctorModel = mongoose.model('doctors', doctorSchema);
+
+module.exports = doctorModel;
