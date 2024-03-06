c5aba483d27cff3c5f04ba14827d6eedb0d2bcba
Lis-prog <lispruthi@gmail.com>
2024-03-06 20:22:53 +0100

    New router for doctor

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(moragan('dev'))
 // Rotuerat
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 
 // Porti Serverit
 const port = process.env.PORT || 8080;
