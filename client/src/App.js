4d1c172f26b31551fe39d0db730c99cbec844f6c
Lis-prog <lispruthi@gmail.com>
2024-03-06 20:23:26 +0100

    Update the code and new routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,92 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import { useSelector } from 'react-redux';
-import Spinner from './components/Spinner';
-import ProtectedRoute from './components/ProtectedRoute';
-import PublicRoute from './components/PublicRoute';
-import ApplyDoctor from './pages/ApplyDoctor';
-import NotifcationPage from './pages/NotifcationPage';
-import Users from './pages/admin/Users';
-import Doctor from './pages/admin/Doctor';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { useSelector } from "react-redux";
+import Spinner from "./components/Spinner";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
+import ApplyDoctor from "./pages/ApplyDoctor";
+import NotifcationPage from "./pages/NotifcationPage";
+import Users from "./pages/admin/Users";
+import Doctor from "./pages/admin/Doctor";
+import Profile from "./pages/doctor/Profile";
 function App() {
-  const {loading} = useSelector(state => state.alerts)
+  const { loading } = useSelector((state) => state.alerts);
   return (
     <>
-    <BrowserRouter>
-    {loading ? 
-    (<Spinner /> ) : (
-        <Routes>
-        <Route path='/' element={
-          <ProtectedRoute>
-            <HomePage />
-          </ProtectedRoute>
-        } 
-        /> 
-        <Route path='/apply-doctor' element={
-          <ProtectedRoute>
-            <ApplyDoctor />
-          </ProtectedRoute>
-        } 
-        /> 
-         <Route path='/admin/users' element={
-          <ProtectedRoute>
-            <Users />
-          </ProtectedRoute>
-        } 
-        /> 
-         <Route path='/admin/doctors' element={
-          <ProtectedRoute>
-            <Doctor />
-          </ProtectedRoute>
-        } 
-        /> 
-        <Route path='/notification' element={
-          <ProtectedRoute>
-            <NotifcationPage />
-          </ProtectedRoute>
-        } 
-        /> 
-        <Route path='/login' element={
-        <PublicRoute>
-          <Login />
-        </PublicRoute>
-        } 
-        /> 
-        <Route path='/register' element={
-        <PublicRoute>
-          <Register />
-        </PublicRoute>
-        } 
-        /> 
-      </Routes>
-    )}
-
-    </BrowserRouter>
+      <BrowserRouter>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/apply-doctor"
+              element={
+                <ProtectedRoute>
+                  <ApplyDoctor />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute>
+                  <Users />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/doctors"
+              element={
+                <ProtectedRoute>
+                  <Doctor />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/doctor/profile/:id"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/notification"
+              element={
+                <ProtectedRoute>
+                  <NotifcationPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              }
+            />
+          </Routes>
+        )}
+      </BrowserRouter>
     </>
   );
 }
