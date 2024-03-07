47d5399033da762b90f4f2ffcc6bb54d5f1d54cf
Lis-prog <lispruthi@gmail.com>
2024-03-08 00:07:59 +0100

    New code add at Homepage of doctorlist

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,17 +1,22 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
+import { Row } from 'antd'
+import DoctorList from '../components/DoctorList'
 import Layout from '../components/Layout'
-
 const HomePage = () => {
 
+  const [doctors, setDoctors] = useState([])
   // Te dhenat e login user 
   const getUserData = async  () => {
     try {
-      const res = await axios.post('/api/v1/user/getUserData', {}, {
+      const res = await axios.get('/api/v1/user/getAllDoctors', {
         headers: {
           Authorization: "Bearer " + localStorage.getItem("token")
         }
-      })
+      });
+      if(res.data.success) {
+        setDoctors(res.data.data)
+      }
     } catch (error) {
       console.log(error)
     }
@@ -21,8 +26,13 @@ const HomePage = () => {
   }, [])
   return (
     <Layout>
-        <h1>Home Page</h1>
-    </Layout>
+        <h1 className='text-center'> Home Page</h1>
+        <Row>
+          {doctors && doctors.map((doctor) => 
+            <DoctorList doctor={doctor} />
+          )}
+        </Row>
+        </Layout>
   )
 }
 
