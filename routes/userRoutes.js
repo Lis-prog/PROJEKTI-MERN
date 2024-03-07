aecc8032fc12dcf69a5552ecf62616144646c7a5
Lis-prog <lispruthi@gmail.com>
2024-03-08 00:11:57 +0100

    New routes for getting doctors and making a appointment to a doctor

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 //Router onject
 const router = express.Router()
 
-const { loginController, registerController, authController, applyDoctorController, getAllNotificationController, deleteAllNotificationController } = require('../controllers/userCtrl');
+const { loginController, registerController, authController, applyDoctorController, getAllNotificationController, deleteAllNotificationController, getAllDoctorsController, bookAppointmentController } = require('../controllers/userCtrl');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 
@@ -27,5 +27,11 @@ router.post('/get-all-notification', authMiddleware, getAllNotificationControlle
 
 // Delete Notification
 router.post('/delete-all-notification', authMiddleware, deleteAllNotificationController);
+
+// Get Doctors
+router.get('/getAllDoctors', authMiddleware, getAllDoctorsController )
+
+// Rezervo termin
+router.post('/book-appointment', authMiddleware, bookAppointmentController)
 
 module.exports = router
