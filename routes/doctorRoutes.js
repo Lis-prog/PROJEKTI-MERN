44cebbf74efa47fd2536f23e36e0cb9afe1119e5
Lis-prog <lispruthi@gmail.com>
2024-03-08 00:11:24 +0100

    New routes for post and update profile of doctor

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,11 +1,34 @@
-const express = require("express");
+// const express = require("express");
 
+// const router = express.Router();
+
+// const authMiddleware = require("../middlewares/authMiddleware");
+// const { getDoctorInfoController, updateProfileController } = require("../controllers/doctorCtrl");
+
+// //  Post single doctor
+// router.post("/getDoctorInfo", authMiddleware, getDoctorInfoController);
+
+// // Post Update profile
+// router.post("/updateProfile", authMiddleware, updateProfileController)
+
+// module.exports = router;
+
+const express = require("express");
+const {
+  getDoctorInfoController,
+  updateProfileController,
+  getDoctorByIdController,
+} = require("../controllers/doctorCtrl");
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-const authMiddleware = require("../middlewares/authMiddleware");
-const { getDoctorInfoController } = require("../controllers/doctorCtrl");
-
-//  Post single doctor
+//POST SINGLE DOC INFO
 router.post("/getDoctorInfo", authMiddleware, getDoctorInfoController);
+
+//POST UPDATE PROFILE
+router.post("/updateProfile", authMiddleware, updateProfileController);
+
+// POST GET Single Doki Info
+router.post("/getDoctorById", authMiddleware, getDoctorByIdController)
 
 module.exports = router;
